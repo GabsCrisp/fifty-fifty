803f7b2c95e7b73b06adc46e36d6bd57b60c3b4d
unknown <eloissemolina@gmail.com>
2024-06-15 23:50:44 -0600

    a

--- static/scripts/register.js
+++ static/scripts/register.js
@@ -1,35 +1,21 @@
-let input = document.querySelectorAll('.input');
-let boton = document.getElementById('registro');
-let form = document.getElementById('form');
-let contador = 0;
-form.addEventListener("submit", function(e){
-    e.preventDefault();
-})
+let inputs = document.getElementsByClassName("input");
 
-let ids = {}
-
-boton.disabled = true;
-for (let i = 0; i < input.length; i++)
-{
-    input[i].addEventListener("input", habilitacion_boton);
-    ids[input[i].id] = 0;
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", chequeo_input);
 }
-
-function habilitacion_boton(e)
+let habilitar = false;
+//revisa si cada uno de los inputs tiene valores y habilita o deshabilta el boton
+function chequeo_input()
 {
-    contador = 0;
-    if (e.target.value.length > 1)
-    {
-        ids[e.target.id] = e.target.value.length;
+    for (let i = 0; i < inputs.length; i++) {
+        if(inputs[i].value < 1)
+            {
+                habilitar = false;
+            }
+            else 
+            {
+                habilitar = true;
+            }
     }
-
-    for (let key in ids)
-    {
-        if (ids[key] > 0)
-        {
-            contador += 1;
-        }
-
-    }
-    console.log(contador);
+    console.log(habilitar);
 }
