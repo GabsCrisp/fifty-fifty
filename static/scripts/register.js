c5a79c35469394c4927ab9de51652272dad63281
GabsCrisp <chanyeollie20@gmail.com>
2024-06-15 22:58:18 -0600

    Verficación de inputs de register efectuados

--- static/scripts/register.js
+++ static/scripts/register.js
@@ -1,35 +1,70 @@
-let input = document.querySelectorAll('.input');
-let boton = document.getElementById('registro');
-let form = document.getElementById('form');
-let contador = 0;
+let inputs = document.getElementsByClassName("input");
+let registro = document.getElementById("registro");
+let form = document.getElementById("form");
+let password = document.getElementById("password");
+let confirmpassword = document.getElementById("confirmpassword");
+let email = document.getElementById("email");
+//previene no recargar el formulario
 form.addEventListener("submit", function(e){
     e.preventDefault();
 })
+registro.addEventListener("click", confirmacion);
 
-let ids = {}
-
-boton.disabled = true;
-for (let i = 0; i < input.length; i++)
-{
-    input[i].addEventListener("input", habilitacion_boton);
-    ids[input[i].id] = 0;
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", chequeo_input);
 }
 
-function habilitacion_boton(e)
-{
-    contador = 0;
-    if (e.target.value.length > 1)
-    {
-        ids[e.target.id] = e.target.value.length;
-    }
+// Funcion chequeo_input: Revisa si cada uno de los inputs tiene valores y habilita o desahabilita el boton
 
-    for (let key in ids)
-    {
-        if (ids[key] > 0)
-        {
-            contador += 1;
+let deshabilitado = true;
+let contador = 0;
+function chequeo_input() {
+    contador = 0;
+    for (let i = 0; i < inputs.length; i++) {
+        //contador se aumenta cada que escribimos en los inputs
+        if (inputs[i].value.length > 0) {
+            contador++;
         }
 
     }
-    console.log(contador);
+    if (contador == inputs.length) {
+
+        registro.disabled = false;
+    }
+    else {
+        registro.disabled = true;
+    }
+}
+function confirmacion() {
+    if(!(email.value.includes("@")))
+        {
+            Swal.fire({
+                title: "El formato del correo no es válido",
+                text: "",
+                icon: "error",
+              });
+              return;
+        }
+    if (password.value == confirmpassword.value)
+        {
+            Swal.fire({
+                title: "¡Registro exitoso!",
+                text: "",
+                icon: "success",
+              });
+              return;
+              //fetch
+              //verificar nombre de usuarios e email en la db
+              //hashear contra
+              //insertar
+        }
+    else
+    {
+        Swal.fire({
+            title: "¡Contraseñas no coinciden!",
+            text: "",
+            icon: "error"
+          });
+          return;
+    }
 }
