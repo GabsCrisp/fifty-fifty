fde1007af1ab270a73a6266a89859cffaad25f45
unknown <eloissemolina@gmail.com>
2024-06-23 00:21:26 -0600

    evento js corregido

--- static/scripts/eventos.js
+++ static/scripts/eventos.js
@@ -1,11 +1,4 @@
 const form = document.getElementById("crear-evento");
-const remove = document.getElementsByClassName("remove");
-const idEvento = document.getElementById("id_evento").value;
-
-for (let i = 0; i < remove.length; i++) {
-    remove[i].addEventListener("click", removeParticipants)
-}
-
 // linea 4 y 5 hacen la misma cosa 
 // form.addEventListener("submit",  (e) => { e.preventDefault(); })
 if (form) {
@@ -36,35 +29,6 @@ function crearEvento() {
             mostrarMensaje(data['status'], data['message'], data['redirect']);
         });
 }
-
-function crearParticipante(event, form) {
-    event.preventDefault();
-
-    const nombreParticipante = form.querySelector("input[name='participante']")
-    const usuarioHidden = form.querySelector("input[name='tipoUsuario']")
-    const info_usuario = {
-        "participante": nombreParticipante.value,
-        "tipoUsuario": usuarioHidden.value
-    };
-    fetch(
-        "/eventos/" + idEvento, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(info_usuario)
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            mostrarMensaje(data['status'], data['message'], data['redirect']);
-        });
-
-}
-
-function irEvento(idEvento) {
-    location.href = 'eventos/' + idEvento;
-}
-
 function mostrarMensaje(status, message, redirect) {
     if (status == 'success') {
         Swal.fire({
@@ -89,26 +53,6 @@ function mostrarMensaje(status, message, redirect) {
             })
     }
 }
-
-function removeParticipants() {
-    //parentNode permite saber el padre de un hijo en html
-    //children nos retorna un arreglo de todos los hijos del padre
-    const children = this.parentNode.children;
-    const username = children[0].innerHTML;
-    const id_participante_evento = children[1].innerHTML;
-
-    info_borrada = { "idEvento": idEvento, "username": username, "id_participante_evento": id_participante_evento }
-    fetch(
-        "/remover_participantes", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(info_borrada)
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            mostrarMensaje(data['status'], data['message'], data['redirect']);
-        });
-
+function irEvento(idEvento) {
+    location.href = 'eventos/' + idEvento;
 }
