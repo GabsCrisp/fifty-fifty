2f7197b91d92113d6b6cce468b4f51d5adead49a
unknown <eloissemolina@gmail.com>
2024-06-28 15:09:46 -0600

    cambios hechos a consumo evento

--- static/scripts/consumoEvento.js
+++ static/scripts/consumoEvento.js
@@ -4,6 +4,50 @@ const invited = document.getElementById("invited");
 const productos = document.getElementById("id_producto");
 const inputPrecio = document.getElementById("id_precio");
 const dataList = document.getElementById("list_product")
+const inputs = document.getElementsByClassName("input");
+const inp = document.getElementsByClassName("inp");
+const agregar_consumo = document.getElementById("agregar_consumo");
+const agregar_producto = document.getElementById("agregar_producto");
+const participantes = document.getElementsByClassName("participantes");
+const precio = document.getElementById("precio");
+const form = document.getElementById("id_form_consumo");
+const opcion_de_agregado = document.getElementById("opcion_de_agregado");
+const botones_enviados = document.getElementsByClassName("botones_enviados");
+
+for (let i = 0; i < botones_enviados.length; i++) {
+    botones_enviados[i].addEventListener("click", (e) => {
+        opcion_de_agregado.value = e.target.innerHTML
+    })
+}
+
+
+//el precio no se estaba enviando correctamente
+//enviamos precio en un input hidden
+form.addEventListener("submit", (e) => {
+    precio.value = inputPrecio.value;
+})
+
+//va contando cuantos checkbox han sido chequeado
+function compartido_por() {
+    let compartido = 0;
+    compartido = 0;
+    for (let i = 0; i < participantes.length; i++) {
+        if (participantes[i].checked) {
+            compartido++
+        }
+
+    }
+    return compartido;
+}
+
+agregar_consumo.disabled = true;
+agregar_producto.disabled = true;
+let encontrado_db = false;
+
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", verificacion_inputs)
+}
+
 
 productos.addEventListener("input", () => {
     const dataList = document.getElementById("list_product");
@@ -12,7 +56,6 @@ productos.addEventListener("input", () => {
     if (productos.value.length === 0) {
         return
     }
-
     fetch(
         `/${idEvento}/producto`, {
         method: 'POST',
@@ -23,33 +66,38 @@ productos.addEventListener("input", () => {
     })
         .then((response) => response.json())
         .then((data) => {
-            console.log(data)
             data.forEach(item => {
                 const option = document.createElement("option");
                 option.value = item[0];
                 option.dataset.price = item[1];
                 dataList.appendChild(option);
             })
+            const options = dataList.children;
+            let matchFound = false;
+            for (let i = 0; i < options.length; i++) {
+                if (options[i].value === productos.value) {
+                    inputPrecio.value = options[i].dataset.price;
+                    encontrado_db = true;
+                    matchFound = true;
+                    verificacion_inputs();
+                    break;
+                }
+                else {
+                    console.log(productos.value);
+                    encontrado_db = false;
+                    verificacion_inputs()
+                }
+            }
+            if (!matchFound) {
+                inputPrecio.value = "";
+            }
+            inputPrecio.disabled = matchFound;
 
         });
 
 })
 
-productos.addEventListener("change", () => {
-    const options = dataList.options;
-    let matchFound = false;
-    for (let i = 0; i < options.length; i++) {
-        if (options[i].value === productos.value) {
-            inputPrecio.value = options[i].dataset.price;
-            matchFound = true;
-            break;
-        }
-    }
-    if (!matchFound) {
-        inputPrecio.value = "";
-    }
-    inputPrecio.disabled = matchFound;
-});
+
 
 
 function mostrarMensaje(status, message, redirect) {
@@ -75,4 +123,27 @@ function mostrarMensaje(status, message, redirect) {
                 window.location = redirect;
             })
     }
+}
+
+function verificacion_inputs() {
+    agregar_consumo.disabled = true
+    agregar_producto.disabled = true
+    let compartido = compartido_por();
+    let contador = 0;
+    for (let i = 0; i < inp.length; i++) {
+        if (inp[i].value.length > 0) {
+            contador++;
+        }
+    }
+    console.log(encontrado_db);
+    //para que se habilite el boton de consumo deben estar rellenados los cuatros inputs
+    //debe haberse encontrado el producto en la db y debe haber por lo menos un checkbox chequeado
+    if (contador == inp.length && encontrado_db && compartido > 0) {
+
+        agregar_consumo.disabled = false;
+    }
+    else if (contador == inp.length && compartido > 0) {
+        agregar_producto.disabled = false;
+    }
+
 }
